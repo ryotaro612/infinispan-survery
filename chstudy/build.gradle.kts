c622a57181f23640cfa8880ebe3cc72d2f003830
Ryotaro <ryotaro612.ooo@gmail.com>
2025-08-11 01:52:31 +0900

    add a modulewipimport

--- chstudy/build.gradle.kts
+++ chstudy/build.gradle.kts
@@ -10,6 +10,15 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.infinispan/infinispan-client-hotrod
+    implementation("org.infinispan:infinispan-client-hotrod:16.0.0.Dev02")
+    // https://mvnrepository.com/artifact/org.infinispan/infinispan-api
+    implementation("org.infinispan:infinispan-api:16.0.0.Dev02")
+    // https://mvnrepository.com/artifact/org.infinispan/infinispan-query-dsl
+    implementation("org.infinispan:infinispan-query-dsl:16.0.0.Dev02")
+    // https://mvnrepository.com/artifact/org.infinispan/infinispan-remote-query-client
+    implementation("org.infinispan:infinispan-remote-query-client:16.0.0.Dev02")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
